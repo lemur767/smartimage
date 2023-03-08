0869b9943698accfd2b84050fd6bd3ac234f893d
Brandon Thomas <lemur767trap@gmail.com>
2023-03-08 17:59:47 -0500

    update to api url

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('172.105.22.223:5000/api/v1/dalle', {
+        const response = await fetch('172.105.22.223/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('172.105.22.223:5000/api/v1/post', {
+        const response = await fetch('172.105.22.223/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
