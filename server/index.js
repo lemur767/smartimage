d4fda83421eaf9a4443fcd189f17ac338f963902
Brandon Thomas <lemur767trap@gmail.com>
2023-03-11 01:27:43 -0500

    route fixes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
-const cors = require("cors")
+
 dotenv.config();
 
 const app = express();
